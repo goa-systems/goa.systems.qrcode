6ec549acc715619825ed28c6fc836ae2616dfa0a
andreasgottardi <andreas.gottardi@gmx.at>
2021-06-15 05:58:05 +0200

    Transfer logic removed completely.

--- src/main/java/goa/systems/qrcode/Generator.java
+++ src/main/java/goa/systems/qrcode/Generator.java
@@ -1,21 +1,10 @@
 package goa.systems.qrcode;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
 import java.util.EnumMap;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,91 +21,10 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import goa.systems.commons.xml.XmlFramework;
-import goa.systems.qrcode.exceptions.WrongDataException;
 
 public class Generator {
 
 	private static final Logger logger = LoggerFactory.getLogger(Generator.class);
-
-	/**
-	 * Generates a minimal sized QR code that can be scaled to any size
-	 * 
-	 * @param tr Transfer data
-	 * @return File object of svg file on disk
-	 */
-	public File generateQrCode(Transfer tr) {
-		File path = new File(System.getProperty("java.io.tmpdir"), String.format("%s.svg", UUID.randomUUID()));
-		String charset = "UTF-8";
-		try (FileOutputStream fos = new FileOutputStream(path)) {
-			String svg = generateSvgString(tr);
-			fos.write(svg.getBytes(Charset.forName(charset)));
-		} catch (IOException e) {
-			logger.error("Error generating QR code.", e);
-		}
-		logger.info("QR code file created successfully in {}", path.getAbsolutePath());
-		logger.info("URL {}", path.toURI());
-		return path;
-	}
-
-	/**
-	 * Generates a minimal sized QR code that can be scaled to any size
-	 * 
-	 * @param tr Transfer data
-	 * @return File object of svg file on disk
-	 */
-	public ByteArrayInputStream generateSvgStream(Transfer tr) {
-
-		StringWriter result = new StringWriter();
-		try {
-			Document d = generateSvgDocument(tr);
-			Transformer t = XmlFramework.getTransformer();
-			t.setOutputProperty(OutputKeys.INDENT, "yes");
-			t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-			t.transform(new DOMSource(d), new StreamResult(result));
-		} catch (TransformerException e) {
-			logger.error("Error generating SVG stream.", e);
-		}
-		logger.info("QR code stream created successfully.");
-		return new ByteArrayInputStream(result.toString().getBytes());
-	}
-
-	/**
-	 * Generates a minimal sized QR code that can be scaled to any size
-	 * 
-	 * @param tr Transfer data
-	 * @return File object of svg file on disk
-	 */
-	public String generateSvgString(Transfer tr) {
-
-		StringWriter result = new StringWriter();
-
-		try {
-			Document d = generateSvgDocument(tr);
-			Transformer t = XmlFramework.getTransformer();
-			t.setOutputProperty(OutputKeys.INDENT, "yes");
-			t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-			t.transform(new DOMSource(d), new StreamResult(result));
-		} catch (TransformerException e) {
-			logger.error("Error generating SVG string.", e);
-		}
-		logger.info("QR code string created successfully.");
-		return result.toString();
-	}
-
-	/**
-	 * Generates a minimal sized QR code that can be scaled to any size
-	 * 
-	 * @param tr Transfer data
-	 * @return File object of svg file on disk
-	 */
-	public Document generateSvgDocument(Transfer tr) {
-		try {
-			return generateSvgDocument(tr.toQRContent());
-		} catch (WrongDataException e) {
-			logger.error("Error in SVG generator.", e);
-			return generateSvgDocument("");
-		}
-	}
 
 	/**
 	 * Generates a minimal sized QR code that can be scaled to any size
--- src/test/java/goa/systems/qrcode/GeneratorTest.java
+++ src/test/java/goa/systems/qrcode/GeneratorTest.java
@@ -2,30 +2,12 @@ package goa.systems.qrcode;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
-
-import goa.systems.commons.xml.XmlFramework;
-import goa.systems.qrcode.exceptions.WrongDataException;
 
 class GeneratorTest {
-
-	private static final Logger logger = LoggerFactory.getLogger(GeneratorTest.class);
 
 	/**
 	 * Tests if base SVG is valid.
@@ -45,82 +27,27 @@ class GeneratorTest {
 	 * deletes the file afterwards.
 	 */
 	@Test
-	void testSvgGeneration() {
-
-		Transfer tr = new Transfer();
-		tr.setBic("00000000000 ");
-		tr.setIban("AT000000000000000000");
-		tr.setCreditor("Prename Surname");
-		tr.setAmount("0000.00");
-		tr.setText("TestTransfer");
-
-		Generator generator = new Generator();
-		String svg = generator.generateSvgString(tr);
-		try {
-			Document d = XmlFramework.getDocumentFromString(svg);
-			assertEquals("svg", d.getFirstChild().getNodeName());
-		} catch (SAXException | IOException | ParserConfigurationException e) {
-			logger.error("Exception occured.", e);
-		}
-	}
-
-	/**
-	 * Generates a SVG QR code into the temporary directory, tests its existence and
-	 * deletes the file afterwards.
-	 */
-	@Test
-	void testStreamGeneration() {
-
-		Transfer tr = new Transfer();
-		tr.setBic("00000000000 ");
-		tr.setIban("AT000000000000000000");
-		tr.setCreditor("Prename Surname");
-		tr.setAmount("0000.00");
-		tr.setText("TestTransfer");
-
-		Generator generator = new Generator();
-		ByteArrayInputStream svg = generator.generateSvgStream(tr);
-		assertNotNull(svg);
-		assertTrue(svg.available() > 0);
-	}
-
-	/**
-	 * Generates a SVG QR code into the temporary directory, tests its existence and
-	 * deletes the file afterwards.
-	 */
-	@Test
 	void testFileGeneration() {
 
-		Transfer tr = new Transfer();
-		tr.setBic("00000000000 ");
-		tr.setIban("AT000000000000000000");
-		tr.setCreditor("Prename Surname");
-		tr.setAmount("0000.00");
-		tr.setText("TestTransfer");
+		//@formatter:off
+		String tr = "BCD\n"
+				+ "001\n"
+				+ "1\n"
+				+ "SCT\n"
+				+ "00000000000 \n"
+				+ "Prename Surname\n"
+				+ "AT000000000000000000\n"
+				+ "0000.00\n"
+				+ "\n"
+				+ "\n"
+				+ "TestTransfer";
+		//@formatter:on
 
 		Generator generator = new Generator();
-		File svg = generator.generateQrCode(tr);
-		assertTrue(svg.exists());
-		svg.delete();
-		assertFalse(svg.exists());
-	}
-
-	/**
-	 * Tests if a exception is thrown when text and reference are set.
-	 */
-	@Test
-	void testWrongDataException() {
-
-		Transfer tr = new Transfer();
-		tr.setBic("00000000000 ");
-		tr.setIban("AT000000000000000000");
-		tr.setCreditor("Prename Surname");
-		tr.setAmount("0000.00");
-		tr.setReference("TestReference");
-		tr.setText("TestText");
-
-		assertThrows(WrongDataException.class, () -> {
-			tr.toQRContent();
+		assertDoesNotThrow(() -> {
+			Document d = generator.generateSvgDocument(tr);
+			Node node = d.getFirstChild();
+			assertEquals("svg", node.getNodeName());
 		});
 	}
 }
