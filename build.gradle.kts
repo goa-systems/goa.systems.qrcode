fb0cd49dd287ecc9a8b71c7f59428b39e2e54484
AndreasGottardi <andreas@goa.systems>
2024-09-17 12:02:29 +0200

    Improving pipeline.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,38 @@
 plugins {
     `java-library`
     `maven-publish`
+    `jacoco`
+}
+
+group = "goa.systems".toString()
+if (hasProperty("ARTIFACT_GROUP")) {
+    group = property("ARTIFACT_GROUP").toString()
+}
+
+var artifactname: String = "qrcode"
+if (hasProperty("ARTIFACT_ID")) {
+    artifactname = property("ARTIFACT_ID").toString()
+}
+
+version = "0.0.0".toString()
+if (hasProperty("ARTIFACT_VERSION")) {
+    version = property("ARTIFACT_VERSION").toString()
 }
 
 // Specify the Java version via property. "11", "17", "21", ...
-val javaversion = project.property("java.version")
-val group = "goa.systems"
-val artifact = "qrcode"
-version = project.property("ARTIFACT_VERSION").toString()
+var javaversion: String = "17"
+if (hasProperty("JAVA_VERSION")) {
+    javaversion = property("JAVA_VERSION").toString()
+}
+
+// Specify the Java version via property. "11", "17", "21", ...
+var savegeneratedimages: Boolean = false
+if (hasProperty("SAVE_GENERATED_IMAGES")) {
+    savegeneratedimages = property("SAVE_GENERATED_IMAGES").toString().toBoolean()
+}
+
+val localreponame = "Project"
+val repodir = "repo"
 
 repositories {
 	mavenCentral()
@@ -27,7 +52,7 @@ dependencies {
     /* https://central.sonatype.com/artifact/org.junit.platform/junit-platform-commons */
 	testImplementation("org.junit.platform:junit-platform-commons:1.11.0")
 	
-	api("goa.systems:commons:0.6.0")
+	implementation("goa.systems:commons:0.6.0")
 	
 	/* Logging https://central.sonatype.com/artifact/org.slf4j/slf4j-api */
 	implementation("org.slf4j:slf4j-api:2.0.16")
@@ -40,90 +65,94 @@ dependencies {
 	testImplementation("ch.qos.logback:logback-classic:1.5.8")
 	
 	/* QR code logic https://central.sonatype.com/artifact/com.google.zxing/core */
-	api("com.google.zxing:core:3.5.3")
-	api("com.google.zxing:javase:3.5.3")
+	implementation("com.google.zxing:core:3.5.3")
+	implementation("com.google.zxing:javase:3.5.3")
 	
 	/* Batik required for testing of svg graphics. https://central.sonatype.com/artifact/org.apache.xmlgraphics/batik */
-	api("org.apache.xmlgraphics:batik:1.17"){
+	implementation("org.apache.xmlgraphics:batik:1.17"){
         exclude(group = "xml-apis", module = "xml-apis")
     }
-	api("org.apache.xmlgraphics:batik-transcoder:1.17"){
+	implementation("org.apache.xmlgraphics:batik-transcoder:1.17"){
         exclude(group = "xml-apis", module = "xml-apis")
     }
 }
 
 java {
+    withSourcesJar()
+    withJavadocJar()
     sourceCompatibility = JavaVersion.valueOf("VERSION_" + javaversion)
     targetCompatibility = JavaVersion.valueOf("VERSION_" + javaversion)
 }
 
 tasks.withType<Test> {
+
+    finalizedBy(tasks.jacocoTestReport)
 	
 	useJUnitPlatform()
 	
 	// Take the system property from the "gradle.properties" file or 
 	// from the -Dsave.generated.images=[true|false] command line parameter.
-	systemProperty("save.generated.images", providers.systemProperty("save.generated.images"))
+	systemProperty("save.generated.images", savegeneratedimages)
 }
 
-tasks.register("writeVariables") {
-    group = "build"
-    description = "Write vars file"
-    doLast {
-        file("build/export/vars").writeText("export VERSION=${version}\nexport GROUP=goa.systems\nexport ARTIFACT=qrcode")
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
     }
 }
-		
-tasks.register<Copy>("copyGeneratedLibraries") {
-    group = "build"
-    description = "Copy generated libraries."
-    dependsOn("jar")
-    dependsOn("sourcesJar")
-    dependsOn("javadocJar")
-    
-    from(layout.buildDirectory.dir("libs"))
-    include("*.jar")
-    into(layout.buildDirectory.dir("export/lib"))
-}
 
-tasks.register<Copy>("copyGeneratedPomFile") {
-    group = "build"
-    description = "Copy generated POM description"
-    dependsOn("generatePomFileForQrcodePublication")
-    
-    from(layout.buildDirectory.file("publications/qrcode/pom-default.xml"))
-    into(layout.buildDirectory.dir("export/conf"))
-}
+tasks.register<Copy>("exportFromLocalRepo"){
 
-java {
-	withSourcesJar()
-	withJavadocJar()
+    group = "build"
+    description = "Exports from local Maven repository"
+    
+    dependsOn(tasks.clean)
+    dependsOn(tasks.get("publish" + artifactname.replaceFirstChar(Char::titlecase) + "PublicationTo" + localreponame + "Repository"))
+    
+    from(layout.buildDirectory.dir(repodir + "/" + project.getGroup().toString().replace(".", "/") + "/" + artifactname + "/" + version))
+    
+    include(artifactname + "-" + version + ".jar")
+    include(artifactname + "-" + version + "-javadoc.jar")
+    include(artifactname + "-" + version + "-sources.jar")
+    include(artifactname + "-" + version + ".pom")
+    
+    into(layout.buildDirectory.dir("test"))
+}
+        
+tasks.register<Tar>("distribute") {
+
+    group = "build"
+    description = "Creates tgz distribution."
+    
+    dependsOn(tasks["exportFromLocalRepo"])
+    
+    from(layout.buildDirectory.dir("test"))
+
+    archiveFileName = artifactname + "-" + version + ".tar.gz"
+    destinationDirectory = layout.buildDirectory.dir("distributions")
+    compression = Compression.GZIP
 }
 
 publishing {
-	publications {
-		create<MavenPublication>(artifact) {
-			groupId = group
-			artifactId = artifact
-			version = version
-			from(components["java"])
-			pom {
-				name = "GOA systems " + artifact
-				description = "A QR code library."
-			}
-		}
-	}
-}  
+
+    repositories {
+        maven {
+            name = localreponame
+            url = uri(layout.buildDirectory.dir(repodir))
+        }
+    }
     
-tasks.register<Tar>("distribute") {
-    group = "build"
-    description = "Create dist"
-    dependsOn("writeVariables")
-    dependsOn("copyGeneratedPomFile")
-    dependsOn("copyGeneratedLibraries")
-    archiveBaseName = rootProject.name
-    destinationDirectory = layout.buildDirectory.dir("distributions")
-    from(layout.buildDirectory.dir("export"))
-    archiveFileName.set("goa.systems.qrcode-${version}.tar.gz")
-    compression = Compression.GZIP
-}   
+    publications {
+        create<MavenPublication>(artifactname) {
+            groupId = group.toString()
+            artifactId = artifactname.toString()
+            version = version
+            from(components["java"])
+            pom {
+                name = "GOA systems " + artifactname
+                description = "A library."
+            }
+        }
+    }
+} 
